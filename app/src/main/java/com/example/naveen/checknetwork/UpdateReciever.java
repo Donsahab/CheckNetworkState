56350fdecc78f9fd2eb371987e9faabdf34e1fee
Donsahab <naveenkryadav007@gmail.com>
2018-10-26 15:04:24 +0530

    first commit

---
+++ app/src/main/java/com/example/naveen/checknetwork/UpdateReciever.java
@@ -0,0 +1,21 @@
+package com.example.naveen.checknetwork;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
+
+public class UpdateReciever extends android.content.BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ConnectivityManager cm =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            Toast.makeText(context, "Network Available", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(context, "Network Not Available", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
